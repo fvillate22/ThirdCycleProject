34bcc3c0b7fb2b12672eff9d1eca146ff6a82c2b
Alexpc1052 <acorreamonroy@gmail.com>
2022-08-14 21:55:57 -0500

    Sprint 2. Objetivo: Crear clases para las entidades básicas de la aplicación.

---
+++ src/Tokens/Empresa.java
@@ -0,0 +1,7 @@
+package Tokens;
+
+public class Empresa {
+    public static void main(String[] args) {
+        System.out.println("Hola compañero, debemos Modelar una empresa como clase java");
+    }
+}
